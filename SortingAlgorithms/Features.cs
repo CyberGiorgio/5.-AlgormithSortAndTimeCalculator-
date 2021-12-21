e9faa6fb36f37f88b472d2f2270921862e02867e
CyberGiorgio <77939560+CyberGiorgio@users.noreply.github.com>
2021-12-21 23:22:39 +0100

    Add files via upload
    C#

---
+++ SortingAlgorithms/Features.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Timers;
+namespace SortingAlgorithms
+{
+    static class Features
+    {
+        public static bool Try { get; private set; }
+        public static int [] GenerateRandom()
+        {
+            Console.WriteLine("How many random numbers do you wish to sort?");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[numbers];     //creation of array
+            for(int i = 0; i < array.Length; i++)
+            {
+                Random rnd = new Random();  //random numbers between 0 and variable numbers
+                array[i] = rnd.Next(0, numbers);
+            }
+            Console.WriteLine("Your random orginal array is : ");
+            return array;
+        }
+        public static void PrintArray(int [] array)
+        {
+            if (array.Length > 100)
+            {
+                Array.Resize(ref array, 100);       // max no. of elements to be printed out
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " , ");
+            }
+            Console.WriteLine("\n");
+        }
+     
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {                   //time counter
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+        }
+        static public bool TryAgain()                                      //end of game, press "y" to restart
+        {
+            Console.WriteLine("\nWould you like to try again? Press 'Y' or any other button to exit");
+            Try = Console.ReadKey().Key == ConsoleKey.Y;
+            if (Try == true)
+            {
+                return true;
+            }
+            else
+            {
+                Environment.Exit(0);
+                return false;                                   //if !"Y" stop the program
+            }
+        }
+        static public void Menu()
+        {
+            Console.WriteLine("Choose one of the following algorithms:\n" +
+                       "[+] 1 BubbleSort\n" +
+                       "[+] 2 InsertionSort\n" +
+                       "[+] 3 MergeSort\n" +
+                       "[+] 4 QuickSort\n" +
+                       "[+] 5 SortVisualStudio\n" +
+                       "   [-] 6 Exit");
+        }
+    }
+}
---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue;
+            int[] array;
+            do
+            {
+                isTrue = false;
+                try
+                {
+                    array = Features.GenerateRandom();
+                    Features.PrintArray(array);
+                    Features.Menu();
+
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {               //choose sorting algorithm
+                        case 1:
+                            Sorting.BubbleSort(array);
+                            Features.PrintArray(array);
+                            break;
+                        case 2:
+                            Sorting.InsertionSort(array);
+                            Features.PrintArray(array);
+                            break;
+                        case 3:
+                            Sorting.MergeSort(array);
+                            Features.PrintArray(array);
+                            break;
+                        case 4:
+                            Sorting.QuickSort(array);
+                            Features.PrintArray(array);
+                            break;
+                        case 5:
+                            Sorting.SortVisualStudio(array);
+                            Features.PrintArray(array);
+                            break;
+                        case 6:
+                            isTrue = true;
+                            System.Environment.Exit(0);
+                            break;
+                    }
+                } catch (FormatException e)
+                {
+                    Console.WriteLine("Input error");
+                }
+                if (Features.TryAgain() == true)            // play again 'Y' or 'N'?   
+                {
+                    Console.Clear();                        //if press "Y" clear screen
+                }
+            } while (!isTrue);
+        }
+    }
+}
